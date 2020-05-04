7216113e986c2626e6faf15fc3e15a8613a92f66
Rajneeshyadav1 <46643894+Rajneeshyadav1@users.noreply.github.com>
2020-05-04 17:40:17 +0530

    Add files via upload

---
+++ src/main/java/com/cts/exception/RestaurantException.java
@@ -0,0 +1,14 @@
+package com.cts.exception;
+
+public class RestaurantException extends Exception {
+
+	
+	 
+	private static final long serialVersionUID = 4227171665478076830L;
+
+	public RestaurantException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
